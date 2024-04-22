f0f2bfab30cf5e21eb1235afaafaa9fb0e85d2d2
Mahathir Islam <mahathirsaad.islam10@gmail.com>
2024-04-22 16:22:03 +0100

    Update models.py
    few models has been added

--- appOne/models.py
+++ appOne/models.py
@@ -1,67 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
 # Create your models here.
 
-
-
-
-
+# Mahathir Saad Islam, w1907417
 
 class Person(models.Model):
-    person_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    date_of_birth = models.DateField(('date of birth'))
-    email = models.EmailField(('email address'), unique=True)
-    phone_number = models.CharField(('phone number'), max_length=17)
-    def __str__(self):
-        return self.person_id
-
-
-class Student(Person):
-    course = models.CharField(max_length=255)
+    date_of_birth = models.DateField(verbose_name='date of birth')
+    email = models.EmailField(verbose_name='email address', unique=True)
+    phone_number = models.CharField(verbose_name='phone number', max_length=17)
 
     def __str__(self):
-        return self.person_id
-
-
-class Admin(Person):
-
-    def __str__(self):
-        return self.person_id
-
-STAFF_TYPES = (
-    ('parttime', 'Part time'),
-    ('fulltime', 'Full time'),
-    ('visitor', 'Visitor'),
-)
-
-class Staff(Person):
-    type = models.CharField(max_length=255, choices=STAFF_TYPES)
-
-    def __str__(self):
-        return self.person_id
-
-
-
-class Reservation(models.Model):
-    reservation_id = models.AutoField(primary_key=True)
-    booking_start_date = models.DateTimeField()
-    booking_end_date = models.DateTimeField()
-    alerts = models.TextField(null=True, blank=True)
-    status = models.CharField(max_length=255, choices=[('approved', 'Approved'), ('rejected', 'Rejected')])
-    purpose = models.TextField(blank=False)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='reservations')
-
-    def __str__(self):
-        return self.reservation_id
+        return f"{self.name} {self.surname}"
 
 class Equipment(models.Model):
-    equipment_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     type_of_device = models.CharField(max_length=255)
     image = models.ImageField(upload_to='appOne/images/')
     quantity = models.PositiveIntegerField()
-    audit = models.DateField()
+    audit = models.DateField(help_text="Please enter the date in YYYY-MM-DD format.")
     device_name = models.CharField(max_length=255)
     status = models.CharField(max_length=255)
     comments = models.TextField(null=True, blank=True)
@@ -69,8 +33,30 @@ class Equipment(models.Model):
     onsite = models.BooleanField(default=True)
     availability = models.BooleanField(default=True)
 
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name='equipment', null=True, blank=True)
-    admin = models.ForeignKey(Admin, on_delete=models.SET_NULL, null=True, blank=True, related_name='equipment')
+    def __str__(self):
+        return f"Equipment {self.id}"
+
+class Reservation(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reservations',blank=True, null=True)
+    booking_start_date = models.DateTimeField()
+    booking_end_date = models.DateTimeField()
+    alerts = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=255, choices=[('approved', 'Approved'), ('rejected', 'Rejected')],blank=True, null=True)
+    purpose = models.TextField()
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE, related_name='reservations', blank=True, null=True)
+    def __str__(self):
+        return f"Reservation {self.id}"
+
+
+class Cart(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='carts')
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE, related_name='cart_items')
+    booking_start_date = models.DateTimeField()
+    booking_end_date = models.DateTimeField()
+    purpose = models.TextField()
+
 
     def __str__(self):
-        return str(self.equipment_id) 
+        return f"Cart {self.id} of {user.first_name} {user.last_name}"
