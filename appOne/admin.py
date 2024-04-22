09e84f342249d8cfff69cf1b8d4e7fd37ebf06dc
Mahathir Islam <mahathirsaad.islam10@gmail.com>
2024-04-22 16:24:45 +0100

    Update admin.py
    Updating registration of models

--- appOne/admin.py
+++ appOne/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+admin.site.register(Cart)
 admin.site.register(Person)
-admin.site.register(Student)
-admin.site.register(Admin)
-admin.site.register(Staff)
 admin.site.register(Reservation)
 admin.site.register(Equipment)
